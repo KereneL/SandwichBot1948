dc93151913b58ecd1054abe7e62241e69517307b
KereneL <kerenel@gmail.com>
2021-07-26 11:06:23 +0300

    Better script

--- script.js
+++ script.js
@@ -1,47 +1,55 @@
 var sandwich = document.getElementById("sandwich-div");
-
-
-var bread = Math.floor(Math.random() * (breadType.length));
 var ingredients = [];
-var addMore = true
+var addMore = true;
+main();
 
-//addIng(breadType[bread]);
-var topBread = document.createElement("li");
-topBread.appendChild(document.createTextNode(breadType[bread]));
-sandwich.appendChild(topBread);
+function createNew(){
+    sandwich.innerHTML = "";
+    ingredients = [];
+    addMore = true;
+    main();
+}
 
-var innerLi = document.createElement("li");
-var inner = document.createElement("ul");
-inner.id = "inner";
-innerLi.appendChild(inner);
-sandwich.appendChild(innerLi);
-inner = document.getElementById("inner");
+function main() {
+    var bread = Math.floor(Math.random() * (breadType.length));
 
-while (addMore == true) {
-    var t = Math.floor(Math.random() * (types.length));
-    var i = Math.floor(Math.random() * (types[t].length));
-    ingredients.push(types[t][i]);
+    //addIng(breadType[bread]);
+    var topBread = document.createElement("li");
+    topBread.appendChild(document.createTextNode(breadType[bread]));
+    sandwich.appendChild(topBread);
 
-    var rnd = (Math.random() * (ingredients.length));
-    console.log(rnd + "," + ingredients.length);
-    if (rnd > 1) {
-        addMore = false;
+    var innerLi = document.createElement("li");
+    var inner = document.createElement("ul");
+    inner.id = "inner";
+    innerLi.appendChild(inner);
+    sandwich.appendChild(innerLi);
+    inner = document.getElementById("inner");
+
+    while (addMore == true) {
+        var t = Math.floor(Math.random() * (types.length));
+        var i = Math.floor(Math.random() * (types[t].length));
+        ingredients.push(types[t][i]);
+
+        var rnd = (Math.random() * (ingredients.length));
+        console.log(rnd + "," + ingredients.length);
+        if (rnd > 1) {
+            addMore = false;
+        }
+    }
+    ingredients.forEach(ing => addIng(ing));
+
+    var bottomBread = document.createElement("li");
+    bottomBread.appendChild(document.createTextNode(breadType[bread]));
+    sandwich.appendChild(bottomBread);
+
+    if (Math.random() < 0.15) {
+        var i = Math.floor(Math.random() * (specialServe.length));
+
+        var serve = document.createElement("li");
+        serve.appendChild(document.createTextNode(`* [${specialServe[i]}]`));
+        sandwich.appendChild(serve);
     }
 }
-ingredients.forEach(ing => addIng(ing));
-
-var bottomBread = document.createElement("li");
-bottomBread.appendChild(document.createTextNode(breadType[bread]));
-sandwich.appendChild(bottomBread);
-
-if (Math.random() < 0.15) {
-    var i = Math.floor(Math.random() * (specialServe.length));
-
-    var serve = document.createElement("li");
-    serve.appendChild(document.createTextNode(`* [${specialServe[i]}]`));
-    sandwich.appendChild(serve);
-}
-
 
 function addIng(ing) {
     var li = document.createElement("li");
